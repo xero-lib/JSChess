1a1c6db8125b4d16bc50609fa55cf5b606433330
xero-lib <jonathankurtz@protonmail.com>
2021-05-20 18:17:48 -0500

    Removed extra file. Correction: Move not fixed

--- util/makeMove.js
+++ util/makeMove.js
@@ -7,7 +7,6 @@ import assignWatches from "../util/assignWatches.js";
 export let moveCount = 0;
 export let turn = "Light";
 export default function makeMove(start, end) {
-	console.log("T&M",turn, moveCount, "and the color is", coordToPiece(start).color)
 	assignWatches();
 	if (coordToPiece(start).color == turn) {
 		let ret = move(coordToLocation(start).piece, end);
