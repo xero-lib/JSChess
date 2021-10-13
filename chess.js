f994f9f2917ecdb04f286d4c4b701c2a07c9a872
J K <jonathankurtz@protonmail.com>
2021-10-13 00:27:34 -0500

    full piece movement functionality (except en passant)

--- chess.js
+++
@@ -1,9 +0,0 @@
-import coordToPiece from "./util/coordToPiece.js";
-import makeMove from "./util/makeMove.js";
-
-makeMove("e2", "e4");
-makeMove("e7", "e5");
-makeMove("d1", "h5");
-makeMove("g7", "g6");
-makeMove("h5", "e5");
-makeMove("a7", "a6");
