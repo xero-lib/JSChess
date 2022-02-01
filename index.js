0acf29da8b49705a8910edde744de59e77e3d411
xero-lib <jonathankurtz@protonmail.com>
2022-01-31 23:56:20 -0600

    Removed debugging strings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import makeMove from "./util/makeMove.js";
 
 /*
     use `makeMove(start, end)` to move from `start` to `end`
-    must be legal moves
+    use `set_FEN("<FEN string>")` to start the board from a specified FEN position
 */
 
 // makeMove("e4", "e5");
