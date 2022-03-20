c10d4492778377252d0b1f5a725084abadb22668
Steam Drive <jonathankurtz@protonmail.com>
2022-03-19 22:22:05 -0500

    git is broken

--- util/persist.js
+++ util/persist.js
@@ -2,8 +2,7 @@
 
 export default function persist(x) {
     if (x === undefined) { return undefined };
-    return JSON.parse(JSON.stringify(x)); //seemingly incompatable with react-build
-    // return Object.assign(Array.isArray(x) ? [] : {}, x) //just doesnt work period
+    return JSON.parse(JSON.stringify(x));
     // return lodash.cloneDeep(x); //not ideal since lodash is big
     
 }
